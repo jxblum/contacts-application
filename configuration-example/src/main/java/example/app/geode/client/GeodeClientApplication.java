4591408892a75018476092a563251eca5272bf96
John Blum <jblum@pivotal.io>
2016-09-09 18:43:49 -0700

    Refactor the GeodeServerApplication to have the CacheServer bind to all NICs on the designated port.
    Create a corresponding Geode ClientCache (cache client) application configuration class.

---
+++ configuration-example/src/main/java/example/app/geode/client/GeodeClientApplication.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package example.app.geode.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
+import com.gemstone.gemfire.cache.GemFireCache;
+import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.client.ClientCacheFactory;
+import com.gemstone.gemfire.cache.client.ClientRegionShortcut;
+import com.gemstone.gemfire.cache.server.CacheServer;
+
+import org.springframework.util.Assert;
+
+/**
+ * The GeodeClientApplication class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+public class GeodeClientApplication implements Runnable {
+
+  protected static final int GEMFIRE_SERVER_PORT = CacheServer.DEFAULT_PORT;
+
+  protected static final String DEFAULT_GEMFIRE_LOG_LEVEL = "config";
+  protected static final String ECHO_REGION_NAME = "Echo";
+  protected static final String GEMFIRE_SERVER_HOST = "localhost";
+
+  public static void main(String[] args) {
+    GeodeClientApplication.run(args);
+  }
+
+  public static GeodeClientApplication newGeodeClientApplication(String[] args) {
+    return new GeodeClientApplication(args);
+  }
+
+  public static GeodeClientApplication run(String[] args) {
+    GeodeClientApplication cacheClientApplication = newGeodeClientApplication(args);
+    cacheClientApplication.run();
+    return cacheClientApplication;
+  }
+
+  private final List<String> arguments;
+
+  private Region<String, String> echo;
+
+  public GeodeClientApplication(String[] args) {
+    this(Arrays.asList(args));
+  }
+
+  public GeodeClientApplication(List<String> args) {
+    Assert.notNull(args, "Program arguments must not be null");
+    this.arguments = args;
+  }
+
+  @Override
+  public void run() {
+    run(this.arguments);
+  }
+
+  @SuppressWarnings("all")
+  void run(List<String> arguments) {
+    ClientCache gemfireCache = null;
+
+    try {
+      gemfireCache = gemfireCache(gemfireProperties());
+      echo = echoRegion(gemfireCache);
+
+      assertThat(echo.getName()).isEqualTo(ECHO_REGION_NAME);
+      assertThat(echo.isEmpty()).isTrue();
+      assertThat(sendEchoRequest("Hello")).isEqualTo("Hello");
+      assertThat(sendEchoRequest("Test")).isEqualTo("Test");
+      assertThat(sendEchoRequest("Good-Bye")).isEqualTo("Good-Bye");
+      assertThat(echo.isEmpty()).isTrue();
+    }
+    catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    finally {
+      close(gemfireCache);
+    }
+  }
+
+  boolean close(GemFireCache cache) {
+    try {
+      if (cache != null) {
+        cache.close();
+        return true;
+      }
+    }
+    catch (Exception ignore) {
+    }
+
+    return false;
+  }
+
+  Properties gemfireProperties() {
+    Properties gemfireProperties = new Properties();
+
+    gemfireProperties.setProperty("name", applicationName());
+    gemfireProperties.setProperty("log-level", logLevel());
+
+    return gemfireProperties;
+  }
+
+  String applicationName() {
+    return GeodeClientApplication.class.getSimpleName();
+  }
+
+  String logLevel() {
+    return System.getProperty("gemfire.log.level", DEFAULT_GEMFIRE_LOG_LEVEL);
+  }
+
+  ClientCache gemfireCache(Properties gemfireProperties) {
+    return new ClientCacheFactory(gemfireProperties)
+      .addPoolServer(GEMFIRE_SERVER_HOST, GEMFIRE_SERVER_PORT)
+      .create();
+  }
+
+  Region<String, String> echoRegion(ClientCache gemfireCache) {
+    return gemfireCache.<String, String>createClientRegionFactory(ClientRegionShortcut.PROXY).create(ECHO_REGION_NAME);
+  }
+
+  String sendEchoRequest(String echoRequest) {
+    String echoResponse = echo.get(echoRequest);
+    System.err.printf("Client said [%1$s]; Server said [%2$s]%n", echoRequest, echoResponse);
+    return echoResponse;
+  }
+}
--- configuration-example/src/main/java/example/app/geode/server/GeodeServerApplication.java
+++ configuration-example/src/main/java/example/app/geode/server/GeodeServerApplication.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -50,181 +51,190 @@ import example.app.geode.cache.loader.EchoCacheLoader;
 @SuppressWarnings("unused")
 public class GeodeServerApplication implements Runnable {
 
-	protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_MAX_CONNECTIONS = 50;
-	protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_MAX_TIME_BETWEEN_PINGS =
-		intValue(TimeUnit.MINUTES.toMillis(5));
-	protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_PORT = 40404;
-	protected static final int DEFAULT_GEMFIRE_JMX_MANAGER_PORT = 1099;
+  protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_MAX_CONNECTIONS = 50;
+  protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_MAX_TIME_BETWEEN_PINGS =
+    intValue(TimeUnit.MINUTES.toMillis(5));
+  protected static final int DEFAULT_GEMFIRE_CACHE_SERVER_PORT = CacheServer.DEFAULT_PORT;
+  protected static final int DEFAULT_GEMFIRE_JMX_MANAGER_PORT = 1099;
 
-	protected static final String DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS = "localhost";
-	protected static final String DEFAULT_GEMFIRE_CACHE_SERVER_HOSTNAME_FOR_CLIENTS =
-		DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS;
-	protected static final String DEFAULT_GEMFIRE_JMX_MANAGER = "true";
-	protected static final String DEFAULT_GEMFIRE_JMX_MANAGER_START = "true";
-	protected static final String DEFAULT_GEMFIRE_EMBEDDED_LOCATOR_HOST_PORT = "localhost[10334]";
-	protected static final String DEFAULT_GEMFIRE_LOG_LEVEL = "config";
+  protected static final String DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS = "";
+  protected static final String DEFAULT_GEMFIRE_CACHE_SERVER_HOSTNAME_FOR_CLIENTS =
+    DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS;
+  protected static final String DEFAULT_GEMFIRE_JMX_MANAGER = "true";
+  protected static final String DEFAULT_GEMFIRE_JMX_MANAGER_START = "true";
+  protected static final String DEFAULT_GEMFIRE_EMBEDDED_LOCATOR_HOST_PORT = "localhost[10334]";
+  protected static final String DEFAULT_GEMFIRE_LOG_LEVEL = "config";
 
-	public static void main(String[] args) {
-		GeodeServerApplication.run(args);
-	}
+  public static void main(String[] args) {
+    GeodeServerApplication.run(args);
+  }
 
-	protected static int intValue(Number value) {
-		return value.intValue();
-	}
+  protected static int intValue(Number value) {
+    return value.intValue();
+  }
 
-	public static GeodeServerApplication newGeodeServerApplication(String[] args) {
-		return new GeodeServerApplication(args);
-	}
+  public static GeodeServerApplication newGeodeServerApplication(String[] args) {
+    return new GeodeServerApplication(args);
+  }
 
-	public static GeodeServerApplication run(String[] args) {
-		GeodeServerApplication geodeServerApplication = newGeodeServerApplication(args);
-		geodeServerApplication.run();
-		return geodeServerApplication;
-	}
+  public static GeodeServerApplication run(String[] args) {
+    GeodeServerApplication peerCacheApplication = newGeodeServerApplication(args);
+    peerCacheApplication.run();
+    return peerCacheApplication;
+  }
 
-	private final AtomicBoolean cacheXmlDriven = new AtomicBoolean(false);
+  private final AtomicBoolean cacheXmlDriven = new AtomicBoolean(false);
 
-	private final List<String> arguments;
+  private final List<String> arguments;
 
-	/**
-	 * Constructs an instance of the {@link GeodeServerApplication} class to configure and fork/launch
-	 * an Apache Geode data server node.
-	 *
-	 * @param args {@link String} array of program arguments.
-	 * @throws NullPointerException if the program arguments are null.
-	 * @see #GeodeServerApplication(List)
-	 */
-	public GeodeServerApplication(String[] args) {
-		this(Arrays.asList(args));
-	}
+  /**
+   * Constructs an instance of the {@link GeodeServerApplication} class to configure and fork/launch
+   * an Apache Geode data server node.
+   *
+   * @param args {@link String} array of program arguments.
+   * @throws NullPointerException if the program arguments are null.
+   * @see #GeodeServerApplication(List)
+   */
+  public GeodeServerApplication(String[] args) {
+    this(Arrays.asList(args));
+  }
 
-	/**
-	 * Constructs an instance of the {@link GeodeServerApplication} class to configure and fork/launch
-	 * an Apache Geode data server node.
-	 *
-	 * @param args {@link List} of program arguments.
-	 * @throws IllegalArgumentException if the program arguments are null.
-	 */
-	public GeodeServerApplication(List<String> args) {
-		Assert.notNull(args, "Program argument must be be null");
-		this.arguments = args;
-	}
+  /**
+   * Constructs an instance of the {@link GeodeServerApplication} class to configure and fork/launch
+   * an Apache Geode data server node.
+   *
+   * @param args {@link List} of program arguments.
+   * @throws IllegalArgumentException if the program arguments are null.
+   */
+  public GeodeServerApplication(List<String> args) {
+    Assert.notNull(args, "Program argument must be be null");
+    this.arguments = args;
+  }
 
-	/* (non-Javadoc) */
-	@Override
-	public void run() {
-		run(arguments);
-	}
+  /* (non-Javadoc) */
+  @Override
+  public void run() {
+    run(this.arguments);
+  }
 
-	protected void run(List<String> arguments) {
-		Cache gemfireCache = null;
+  protected void run(List<String> arguments) {
+    Cache gemfireCache = null;
 
-		try {
-			if (hasArguments(arguments)) {
-				cacheXmlDriven.set(true);
-				gemfireCache = gemfireCache(gemfireProperties(), arguments.get(0));
-			}
-			else {
-				gemfireCache = gemfireCache(gemfireProperties());
-			}
+    try {
+      if (hasArguments(arguments)) {
+        cacheXmlDriven.set(true);
+        gemfireCache = gemfireCache(gemfireProperties(), arguments.get(0));
+      }
+      else {
+        gemfireCache = gemfireCache(gemfireProperties());
+      }
 
-			gemfireCacheServer(gemfireCache);
-			echoRegion(gemfireCache);
-		}
-		catch (Exception uhOh) {
-			uhOh.printStackTrace(System.err);
-			close(gemfireCache);
-		}
-	}
+      gemfireCacheServer(gemfireCache);
+      echoRegion(gemfireCache);
+      waitOnInput();
+    }
+    catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+    finally {
+      close(gemfireCache);
+    }
+  }
 
-	boolean hasArguments(Object... arguments) {
-		return (arguments != null && arguments.length > 0);
-	}
+  boolean hasArguments(Object... arguments) {
+    return (arguments != null && arguments.length > 0);
+  }
 
-	boolean hasArguments(List<?> arguments) {
-		return (arguments != null && !arguments.isEmpty());
-	}
+  boolean hasArguments(List<?> arguments) {
+    return (arguments != null && !arguments.isEmpty());
+  }
 
-	boolean close(GemFireCache cache) {
-		try {
-			if (cache != null) {
-				cache.close();
-				return true;
-			}
-		}
-		catch (Exception ignore) {
-		}
+  boolean close(GemFireCache cache) {
+    try {
+      if (cache != null) {
+        cache.close();
+        return true;
+      }
+    }
+    catch (Exception ignore) {
+    }
 
-		return false;
-	}
+    return false;
+  }
 
-	String applicationName() {
-		return String.format("%1$s%2$s", (cacheXmlDriven.get() ? "CacheXmlBased" : ""),
-			GeodeServerApplication.class.getSimpleName());
-	}
+  void waitOnInput() {
+    System.out.println("Press <enter> to exit.");
+    Scanner in = new Scanner(System.in);
+    in.nextLine();
+  }
 
-	boolean jmxManager() {
-		return Boolean.parseBoolean(System.getProperty("gemfire.manager.enabled", DEFAULT_GEMFIRE_JMX_MANAGER));
-	}
+  String applicationName() {
+    return String.format("%1$s%2$s", (cacheXmlDriven.get() ? "CacheXmlBased" : ""),
+      GeodeServerApplication.class.getSimpleName());
+  }
 
-	int jmxManagerPort() {
-		return Integer.getInteger("gemfire.manager.port", DEFAULT_GEMFIRE_JMX_MANAGER_PORT);
-	}
+  boolean jmxManager() {
+    return Boolean.parseBoolean(System.getProperty("gemfire.manager.enabled", DEFAULT_GEMFIRE_JMX_MANAGER));
+  }
 
-	boolean jmxManagerStart() {
-		return Boolean.parseBoolean(System.getProperty("gemfire.manager.start", DEFAULT_GEMFIRE_JMX_MANAGER_START));
-	}
+  int jmxManagerPort() {
+    return Integer.getInteger("gemfire.manager.port", DEFAULT_GEMFIRE_JMX_MANAGER_PORT);
+  }
 
-	String logLevel() {
-		return System.getProperty("gemfire.log.level", DEFAULT_GEMFIRE_LOG_LEVEL);
-	}
+  boolean jmxManagerStart() {
+    return Boolean.parseBoolean(System.getProperty("gemfire.manager.start", DEFAULT_GEMFIRE_JMX_MANAGER_START));
+  }
 
-	String startLocator() {
-		return System.getProperty("gemfire.locator.embedded.host-port", DEFAULT_GEMFIRE_EMBEDDED_LOCATOR_HOST_PORT);
-	}
+  String logLevel() {
+    return System.getProperty("gemfire.log.level", DEFAULT_GEMFIRE_LOG_LEVEL);
+  }
 
-	Properties gemfireProperties() {
-		Properties gemfireProperties = new Properties();
+  String startLocator() {
+    return System.getProperty("gemfire.locator.embedded.host-port", DEFAULT_GEMFIRE_EMBEDDED_LOCATOR_HOST_PORT);
+  }
 
-		gemfireProperties.setProperty("name", applicationName());
-		gemfireProperties.setProperty("mcast-port", "0");
-		gemfireProperties.setProperty("log-level", logLevel());
-		gemfireProperties.setProperty("jmx-manager", String.valueOf(jmxManager()));
-		gemfireProperties.setProperty("jmx-manager-port", String.valueOf(jmxManagerPort()));
-		gemfireProperties.setProperty("jmx-manager-start", String.valueOf(jmxManagerStart()));
-		gemfireProperties.setProperty("start-locator", startLocator());
+  Properties gemfireProperties() {
+    Properties gemfireProperties = new Properties();
 
-		return gemfireProperties;
-	}
+    gemfireProperties.setProperty("name", applicationName());
+    gemfireProperties.setProperty("mcast-port", "0");
+    gemfireProperties.setProperty("log-level", logLevel());
+    gemfireProperties.setProperty("jmx-manager", String.valueOf(jmxManager()));
+    gemfireProperties.setProperty("jmx-manager-port", String.valueOf(jmxManagerPort()));
+    gemfireProperties.setProperty("jmx-manager-start", String.valueOf(jmxManagerStart()));
+    gemfireProperties.setProperty("start-locator", startLocator());
 
-	Cache gemfireCache(Properties gemfireProperties) {
-		return new CacheFactory(gemfireProperties).create();
-	}
+    return gemfireProperties;
+  }
 
-	Cache gemfireCache(Properties gemfireProperties, String cacheXmlPathname) {
-		return new CacheFactory(gemfireProperties).set("cache-xml-file", cacheXmlPathname).create();
-	}
+  Cache gemfireCache(Properties gemfireProperties) {
+    return new CacheFactory(gemfireProperties).create();
+  }
 
-	CacheServer gemfireCacheServer(Cache gemfireCache) throws IOException {
-		CacheServer gemfireCacheServer = gemfireCache.addCacheServer();
+  Cache gemfireCache(Properties gemfireProperties, String cacheXmlPathname) {
+    return new CacheFactory(gemfireProperties).set("cache-xml-file", cacheXmlPathname).create();
+  }
 
-		gemfireCacheServer.setBindAddress(DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS);
-		gemfireCacheServer.setHostnameForClients(DEFAULT_GEMFIRE_CACHE_SERVER_HOSTNAME_FOR_CLIENTS);
-		gemfireCacheServer.setMaxConnections(DEFAULT_GEMFIRE_CACHE_SERVER_MAX_CONNECTIONS);
-		gemfireCacheServer.setMaximumTimeBetweenPings(DEFAULT_GEMFIRE_CACHE_SERVER_MAX_TIME_BETWEEN_PINGS);
-		gemfireCacheServer.setPort(DEFAULT_GEMFIRE_CACHE_SERVER_PORT);
-		gemfireCacheServer.start();
+  CacheServer gemfireCacheServer(Cache gemfireCache) throws IOException {
+    CacheServer gemfireCacheServer = gemfireCache.addCacheServer();
 
-		return gemfireCacheServer;
-	}
+    gemfireCacheServer.setBindAddress(DEFAULT_GEMFIRE_CACHE_SERVER_BIND_ADDRESS);
+    gemfireCacheServer.setHostnameForClients(DEFAULT_GEMFIRE_CACHE_SERVER_HOSTNAME_FOR_CLIENTS);
+    gemfireCacheServer.setMaxConnections(DEFAULT_GEMFIRE_CACHE_SERVER_MAX_CONNECTIONS);
+    gemfireCacheServer.setMaximumTimeBetweenPings(DEFAULT_GEMFIRE_CACHE_SERVER_MAX_TIME_BETWEEN_PINGS);
+    gemfireCacheServer.setPort(DEFAULT_GEMFIRE_CACHE_SERVER_PORT);
+    gemfireCacheServer.start();
 
-	Region<String, String> echoRegion(Cache gemfireCache) {
-		RegionFactory<String, String> echoRegion = gemfireCache.createRegionFactory(RegionShortcut.PARTITION);
+    return gemfireCacheServer;
+  }
 
-		echoRegion.setCacheLoader(EchoCacheLoader.getInstance());
-		echoRegion.setKeyConstraint(String.class);
-		echoRegion.setValueConstraint(String.class);
+  Region<String, String> echoRegion(Cache gemfireCache) {
+    RegionFactory<String, String> echoRegion = gemfireCache.createRegionFactory(RegionShortcut.PARTITION);
 
-		return echoRegion.create("Echo");
-	}
+    echoRegion.setCacheLoader(EchoCacheLoader.getInstance());
+    echoRegion.setKeyConstraint(String.class);
+    echoRegion.setValueConstraint(String.class);
+
+    return echoRegion.create("Echo");
+  }
 }
