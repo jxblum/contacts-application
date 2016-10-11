1730b6318eb4849dce5850fa58b29fc62b394404
John Blum <jblum@pivotal.io>
2016-10-11 11:54:22 -0700

    Include examples of Apache Geode with Spring Data Geode configuration compared with using Hazelcast's API.

---
+++ configuration-example/src/test/java/example/config/geode/api/HazelcastApiExampleTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package example.config.geode.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.gemstone.gemfire.cache.Cache;
+import com.gemstone.gemfire.cache.CacheFactory;
+import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.cache.RegionShortcut;
+
+/**
+ * The {@link HazelcastApiExampleTests} class demonstrates how to implement the Hazelcast.org's
+ * example applications using the Hazelcast API (e.g. {@literal DistributedMap}
+ * with Apache Geode's API.
+ *
+ * @author John Blum
+ * @see <a href="http://geode.incubator.apache.org/releases/latest/javadoc/index.html">Apache Geode API</a>
+ * @see <a href="https://hazelcast.org/">Hazelcast API Examples</a>
+ * @since 1.0.0
+ */
+public class HazelcastApiExampleTests {
+
+  private Cache gemfireCache;
+
+  private Region<String, String> myDistributedMap;
+
+  @Before
+  public void setup() {
+    gemfireCache = new CacheFactory().create();
+    myDistributedMap = gemfireCache.<String, String>createRegionFactory(RegionShortcut.LOCAL)
+      .create("my-distributed-map");
+  }
+
+  @After
+  public void tearDown() {
+    if (gemfireCache != null) {
+      gemfireCache.close();
+    }
+  }
+
+  @Test
+  public void distributeMapOperations() {
+    assertThat(myDistributedMap.put("key", "value")).isNull();
+    assertThat(myDistributedMap.get("key")).isEqualTo("value");
+    assertThat(myDistributedMap.putIfAbsent("somekey", "somevalue")).isNull();
+    assertThat(myDistributedMap.replace("key", "value", "newvalue")).isTrue();
+  }
+}
---
+++ configuration-example/src/test/java/example/config/spring/java/HazelcastApiExampleTests.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package example.config.spring.java;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.gemstone.gemfire.cache.GemFireCache;
+import com.gemstone.gemfire.cache.Region;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.CacheFactoryBean;
+import org.springframework.data.gemfire.LocalRegionFactoryBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * The {@link HazelcastApiExampleTests} class demonstrates how to implement the Hazelcast.org's
+ * example applications using the Hazelcast API (e.g. {@literal DistributedMap}
+ * with Spring Data Geode's API (Java-based configuration meta-data).
+ *
+ * @author John Blum
+ * @see <a href="http://docs.spring.io/spring-data-gemfire/docs/current/api/">Spring Data Geode API</a>
+ * @see <a href="https://hazelcast.org/">Hazelcast API Examples</a>
+ * @since 1.0.0
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@SuppressWarnings("unused")
+public class HazelcastApiExampleTests {
+
+  @Resource(name = "myDistributedRegion")
+  private Region<String, String> myDistributedMap;
+
+  @Test
+  public void distributedMapOperations() {
+    assertThat(myDistributedMap.put("key", "value")).isNull();
+    assertThat(myDistributedMap.get("key")).isEqualTo("value");
+    assertThat(myDistributedMap.putIfAbsent("somekey", "somevalue")).isNull();
+    assertThat(myDistributedMap.replace("key", "value", "newvalue")).isTrue();
+  }
+
+  @Configuration
+  static class HazelcastApiExampleConfiguration {
+
+    @Bean
+    CacheFactoryBean gemfireCache() {
+      CacheFactoryBean gemfireCache = new CacheFactoryBean();
+      gemfireCache.setClose(true);
+      return gemfireCache;
+    }
+
+    @Bean
+    LocalRegionFactoryBean<String, String> myDistributedRegion(GemFireCache gemfireCache) {
+      LocalRegionFactoryBean<String, String> myDistributedRegion = new LocalRegionFactoryBean<>();
+      myDistributedRegion.setCache(gemfireCache);
+      myDistributedRegion.setClose(false);
+      return myDistributedRegion;
+    }
+  }
+}
