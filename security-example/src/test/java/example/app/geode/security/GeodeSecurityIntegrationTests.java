5b90bb925934be7abc305d1d5559ca7320c7952e
John Blum <jblum@pivotal.io>
2016-11-09 17:04:26 -0800

    Add test cases to proxy the Apache Geode SecurityManager interface and use multiple, ordered and provided Apache Shiro Realms.

--- security-example/src/test/java/example/app/geode/security/GeodeSecurityIntegrationTests.java
+++ security-example/src/test/java/example/app/geode/security/GeodeSecurityIntegrationTests.java
@@ -26,6 +26,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -42,16 +43,25 @@ import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.cache.client.ServerOperationException;
 import org.apache.geode.internal.security.shiro.GeodePermissionResolver;
 import org.apache.geode.security.NotAuthorizedException;
+import org.apache.shiro.authc.Authenticator;
+import org.apache.shiro.authc.pam.AllSuccessfulStrategy;
+import org.apache.shiro.authc.pam.AtLeastOneSuccessfulStrategy;
+import org.apache.shiro.authc.pam.AuthenticationStrategy;
+import org.apache.shiro.authc.pam.FirstSuccessfulStrategy;
+import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
+import org.apache.shiro.mgt.AuthenticatingSecurityManager;
 import org.apache.shiro.realm.Realm;
+import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.realm.text.PropertiesRealm;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.data.gemfire.LocalRegionFactoryBean;
 import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
 import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
@@ -59,18 +69,25 @@ import org.springframework.data.gemfire.config.annotation.ClientCacheApplication
 import org.springframework.data.gemfire.config.annotation.EnableAuth;
 import org.springframework.data.gemfire.config.annotation.EnableManager;
 import org.springframework.data.gemfire.config.annotation.EnableSecurity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import example.app.core.bean.factory.config.BeanPostProcessorSupport;
 import example.app.core.util.CollectionUtils;
 import example.app.geode.cache.loader.EchoCacheLoader;
 import example.app.geode.security.GeodeSecurityIntegrationTests.GeodeClientConfiguration;
 import example.app.geode.security.model.User;
+import example.app.geode.security.provider.SimpleSecurityManager;
 import example.app.geode.security.repository.SecurityRepository;
+import example.app.geode.security.repository.support.JdbcSecurityRepository;
 import example.app.geode.security.repository.support.XmlSecurityRepository;
 import example.app.geode.tests.integration.AbstractGeodeIntegrationTests;
+import example.app.shiro.authc.pam.ShiroAuthenticationStrategy;
 import example.app.shiro.realm.SecurityRepositoryAuthorizingRealm;
 
 /**
@@ -107,6 +124,7 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
   protected static final AtomicInteger TEST_CASE_COUNT = new AtomicInteger(0);
 
   protected static final String CACHE_SERVER_HOST = "localhost";
+  protected static final String GEODE_LOG_LEVEL = "config";
 
   private static Process geodeServer;
 
@@ -114,8 +132,10 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
   public static void runGeodeServer() throws IOException {
     geodeServer = run(GeodeServerConfiguration.class,
       String.format("-Dgemfire.log-level=%s", logLevel()),
+      String.format("-Dshiro.authentication.strategy=%s",
+        systemProperty("shiro.authentication.strategy", "atLeastOneSuccessful")),
       String.format("-Dspring.profiles.active=apache-geode-server,%s",
-        System.getProperty("security-example-profile", "apache-geode-security")));
+        systemProperty("security-example-profile", "geode-security-manager-proxy-configuration")));
 
     waitForServerToStart(geodeServer, CACHE_SERVER_HOST, CACHE_SERVER_PORT);
   }
@@ -125,10 +145,12 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
     stop(geodeServer);
   }
 
+  protected static String systemProperty(String propertyName, String defaultValue) {
+    return System.getProperty(propertyName, defaultValue);
+  }
+
   @Rule
   public ExpectedException exception = ExpectedException.none();
-
-  protected final Logger logger = LoggerFactory.getLogger(getClass());
 
   @Resource(name = "Echo")
   private Region<String, String> echo;
@@ -152,7 +174,7 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
     echo.put("two", "four");
   }
 
-  @ClientCacheApplication(name = "GeodeSecurityIntegrationTestsClient", logLevel = "config",
+  @ClientCacheApplication(name = "GeodeSecurityIntegrationTestsClient", logLevel = GEODE_LOG_LEVEL,
     pingInterval = CLIENT_CACHE_PING_INTERVAL, readTimeout = CLIENT_CACHE_READ_TIMEOUT,
     servers = { @ClientCacheApplication.Server(port = CACHE_SERVER_PORT) })
   @EnableAuth(clientAuthenticationInitializer = "example.app.geode.security.GeodeClientAuthInitialize.create")
@@ -171,11 +193,17 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
     }
   }
 
-  @CacheServerApplication(name = "GeodeSecurityIntegrationTestsServer", logLevel = "config",
-    maxTimeBetweenPings = CACHE_SERVER_MAX_TIME_BETWEEN_PINGS, port = CACHE_SERVER_PORT)
+  @CacheServerApplication(name = "GeodeSecurityIntegrationTestsServer", logLevel = GEODE_LOG_LEVEL,
+    maxTimeBetweenPings = CACHE_SERVER_MAX_TIME_BETWEEN_PINGS, port = CACHE_SERVER_PORT, useBeanFactoryLocator = true)
   @EnableManager(start = true)
-  @Import({ GeodeIntegratedSecurityConfiguration.class, ApacheShiroIniConfiguration.class,
-    ApacheShiroCustomRealmConfiguration.class, ApacheShiroProvidedRealmConfiguration.class })
+  @Import({
+    GeodeIntegratedSecurityConfiguration.class,
+    GeodeIntegratedSecurityProxyConfiguration.class,
+    ApacheShiroIniConfiguration.class,
+    ApacheShiroCustomRealmConfiguration.class,
+    ApacheShiroProvidedRealmConfiguration.class,
+    ApacheShiroProvidedOrderedMultiRealmConfiguration.class
+  })
   @Profile("apache-geode-server")
   public static class GeodeServerConfiguration {
 
@@ -200,7 +228,7 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
 
     @PostConstruct
     public void postProcess() {
-      System.err.printf("Geode Distributed System Properties [%s]%n",
+      logger.info("Geode Distributed System Properties [{}]%n",
         CollectionUtils.toString(gemfireCache.getDistributedSystem().getProperties()));
     }
   }
@@ -211,6 +239,45 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
   @EnableSecurity(securityManagerClassName = "example.app.geode.security.provider.SimpleSecurityManager")
   @Profile("geode-security-manager-configuration")
   static class GeodeIntegratedSecurityConfiguration {
+  }
+
+  @Configuration
+  @EnableSecurity(securityManagerClassName = "example.app.geode.security.SecurityManagerProxy")
+  @Profile("geode-security-manager-proxy-configuration")
+  static class GeodeIntegratedSecurityProxyConfiguration {
+
+    @Bean
+    DataSource hsqlDataSource() {
+      return new EmbeddedDatabaseBuilder()
+        .setName("geode_security")
+        .setScriptEncoding("UTF-8")
+        .setType(EmbeddedDatabaseType.HSQL)
+        .addScript("geode-security-schema-ddl.sql")
+        .addScript("define-roles-table-ddl.sql")
+        .addScript("define-roles-permissions-table-ddl.sql")
+        .addScript("define-users-table-ddl.sql")
+        .addScript("define-users-roles-table-ddl.sql")
+        .addScript("insert-roles-dml.sql")
+        .addScript("insert-roles-permissions-dml.sql")
+        .addScript("insert-users-dml.sql")
+        .addScript("insert-users-roles-dml.sql")
+        .build();
+    }
+
+    @Bean
+    JdbcTemplate hsqlTemplate(DataSource hsqlDataSource) {
+      return new JdbcTemplate(hsqlDataSource);
+    }
+
+    @Bean
+    JdbcSecurityRepository securityRepository(JdbcTemplate hsqlTemplate) {
+      return new JdbcSecurityRepository(hsqlTemplate);
+    }
+
+    @Bean
+    SimpleSecurityManager securityManager(SecurityRepository<User> securityRepository) {
+      return new SimpleSecurityManager(securityRepository);
+    }
   }
 
   @Configuration
@@ -244,6 +311,75 @@ public class GeodeSecurityIntegrationTests extends AbstractGeodeIntegrationTests
     PropertiesRealm shiroRealm() {
       PropertiesRealm propertiesRealm = new PropertiesRealm();
       propertiesRealm.setResourcePath("classpath:shiro.properties");
+      propertiesRealm.setPermissionResolver(new GeodePermissionResolver());
+      return propertiesRealm;
+    }
+  }
+
+  @Configuration
+  @EnableSecurity
+  @Profile("shiro-provided-ordered-multi-realm-configuration")
+  static class ApacheShiroProvidedOrderedMultiRealmConfiguration {
+
+    @Bean
+    BeanPostProcessor shiroSecurityManagerPostProcessor() {
+      return new BeanPostProcessorSupport() {
+        @Override public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+          if ("shiroSecurityManager".equals(beanName) && bean instanceof AuthenticatingSecurityManager) {
+            Authenticator authenticator = ((AuthenticatingSecurityManager) bean).getAuthenticator();
+
+            if (authenticator instanceof ModularRealmAuthenticator) {
+              ((ModularRealmAuthenticator) authenticator).setAuthenticationStrategy(authenticationStrategyResolver());
+            }
+          }
+
+          return bean;
+        }
+      };
+    }
+
+    @SuppressWarnings("all")
+    private AuthenticationStrategy authenticationStrategyResolver() {
+      String authenticationStategyName = systemProperty("shiro.authentication.strategy", "atLeastOneSuccessful");
+
+      ShiroAuthenticationStrategy authenticationStrategy =
+        ShiroAuthenticationStrategy.findBy(authenticationStategyName);
+
+      switch (authenticationStrategy) {
+        case ALL_SUCCESSFUL:
+          return allSuccessfulStrategy();
+        case FIRST_SUCCESSFUL:
+          return firstSuccessfulStrategy();
+        default:
+          return atLeastOneSuccessfulStrategy();
+      }
+    }
+
+    private AllSuccessfulStrategy allSuccessfulStrategy() {
+      return new AllSuccessfulStrategy();
+    }
+
+    private AtLeastOneSuccessfulStrategy atLeastOneSuccessfulStrategy() {
+      return new AtLeastOneSuccessfulStrategy();
+    }
+
+    private FirstSuccessfulStrategy firstSuccessfulStrategy() {
+      return new FirstSuccessfulStrategy();
+    }
+
+    @Bean
+    @Order(1)
+    IniRealm iniRealm() {
+      IniRealm iniRealm = new IniRealm("classpath:partial-shiro.ini");
+      iniRealm.setPermissionResolver(new GeodePermissionResolver());
+      return iniRealm;
+    }
+
+    @Bean
+    @Order(2)
+    PropertiesRealm propertiesRealm() {
+      PropertiesRealm propertiesRealm = new PropertiesRealm();
+      propertiesRealm.setResourcePath("classpath:partial-shiro.properties");
       propertiesRealm.setPermissionResolver(new GeodePermissionResolver());
       return propertiesRealm;
     }
