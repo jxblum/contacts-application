76d1b1a5faec45d48b1c6c95351901a49c9fbb0f
John Blum <jblum@pivotal.io>
2018-01-18 07:52:07 -0800

    Simplify the configuration of the Caching Example.
    Add a Spring Boot ApplicationRunner to the application to geocode some Addresses.

--- caching-example/src/main/java/example/app/CachingExampleApplication.java
+++ caching-example/src/main/java/example/app/CachingExampleApplication.java
@@ -16,8 +16,17 @@
 
 package example.app;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.FilterType;
+
+import example.app.core.lang.RunnableUtils;
+import example.app.model.Address;
+import example.app.service.GeocodingService;
 
 /**
  * The CachingExampleApplication class is a {@link SpringBootApplication} demonstrating Spring's Cache Abstraction
@@ -29,9 +38,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 1.0.0
  */
 @SpringBootApplication
+@ComponentScan(excludeFilters = @Filter(type = FilterType.REGEX, pattern = "example\\.app\\.geode\\.security\\..*"))
 public class CachingExampleApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(CachingExampleApplication.class, args);
+  }
+
+  @Bean
+  @SuppressWarnings("unused")
+  ApplicationRunner runner(GeocodingService geocodingService) {
+
+    return args -> {
+
+      Address pivotal = Address.parse("15220 NW Greenbrier Pkwy, Beaverton, OR, 97006");
+
+      RunnableUtils.timedRun(() ->
+        System.err.printf("Address [%1$s] is at [%2$s]%n", pivotal, geocodingService.geocode(pivotal))
+      ).ifPresent(milliseconds -> System.err.printf("%d ms%n", milliseconds));
+
+      Address ebay = Address.parse("1400 Southwest 5th Avenue, Portland, OR, 97201");
+
+      RunnableUtils.timedRun(() ->
+        System.err.printf("Address [%1$s] is at [%2$s]%n", ebay, geocodingService.geocode(ebay))
+      ).ifPresent(milliseconds -> System.err.printf("%d ms%n", milliseconds));
+
+      RunnableUtils.timedRun(() ->
+        System.err.printf("Address [%1$s] is at [%2$s]%n", pivotal, geocodingService.geocode(pivotal))
+      ).ifPresent(milliseconds -> System.err.printf("%d ms%n", milliseconds));
+    };
   }
 }
--- caching-example/src/main/java/example/app/config/CachingExampleConfiguration.java
+++ caching-example/src/main/java/example/app/config/CachingExampleConfiguration.java
@@ -16,15 +16,10 @@
 
 package example.app.config;
 
-import org.springframework.context.annotation.Bean;
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.config.annotation.ClientCacheApplication;
 import org.springframework.data.gemfire.config.annotation.EnableCachingDefinedRegions;
-import org.springframework.data.gemfire.config.annotation.PeerCacheApplication;
-
-import attic.app.config.gemfire.GemFireConfiguration;
-import example.app.repo.GeocodingRepository;
-import example.app.repo.provider.GoogleMapsApiGeocodingRepository;
-import example.app.service.GeocodingService;
 
 /**
  * The {@link CachingExampleConfiguration} class is the Contacts Application, Caching Example
@@ -32,30 +27,13 @@ import example.app.service.GeocodingService;
  * as the [JSR-107] caching provider.
  *
  * @author John Blum
- * @see GemFireConfiguration
- * @see example.app.repo.GeocodingRepository
- * @see example.app.repo.provider.GoogleMapsApiGeocodingRepository
- * @see example.app.service.GeocodingService
- * @see org.springframework.cache.annotation.EnableCaching
- * @see org.springframework.context.annotation.Bean
- * @see org.springframework.context.annotation.Configuration
- * @see org.springframework.context.annotation.Import
  * @see org.springframework.data.gemfire.cache.GemfireCacheManager
+ * @see org.springframework.data.gemfire.cache.config.EnableGemfireCaching
  * @since 1.0.0
  */
-@PeerCacheApplication(name = "CachingExampleApplication")
-@EnableCachingDefinedRegions
+@ClientCacheApplication(name = "CachingExampleApplication")
+@EnableCachingDefinedRegions(clientRegionShortcut = ClientRegionShortcut.LOCAL)
 @SuppressWarnings("unused")
 public class CachingExampleConfiguration {
 
-  @Bean
-  @SuppressWarnings("unchecked")
-  public <T extends GeocodingRepository> T geocodingRepository() {
-    return (T) new GoogleMapsApiGeocodingRepository();
-  }
-
-  @Bean
-  public GeocodingService geocodingService() {
-    return new GeocodingService(geocodingRepository());
-  }
 }
