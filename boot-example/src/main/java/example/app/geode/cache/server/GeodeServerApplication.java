a78b81c17e9eb6e23e4d5d228f70cf228f8a192a
John Blum <jblum@pivotal.io>
2018-06-03 20:26:49 -0700

    Add class to confiugure and bootstrap an Apache Geode server with Spring Boot.

---
+++ boot-example/src/main/java/example/app/geode/cache/server/GeodeServerApplication.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package example.app.geode.cache.server;
+
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.server.CacheServer;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.EnableLocator;
+import org.springframework.data.gemfire.config.annotation.EnableManager;
+import org.springframework.data.gemfire.config.annotation.EnablePdx;
+
+/**
+ * The GeodeServerApplication class is a {@link SpringBootApplication} that configures and bootstraps an Apache Geode
+ * {@link Cache peer Cache} instance, running with a {@link CacheServer} to allow connections from cache client
+ * applications.
+ *
+ * @author John Blum
+ * @see org.apache.geode.cache.Cache
+ * @see org.apache.geode.cache.server.CacheServer
+ * @see org.springframework.boot.autoconfigure.SpringBootApplication
+ * @see org.springframework.boot.builder.SpringApplicationBuilder
+ * @see org.springframework.data.gemfire.config.annotation.CacheServerApplication
+ * @see org.springframework.data.gemfire.config.annotation.EnableLocator
+ * @see org.springframework.data.gemfire.config.annotation.EnableManager
+ * @see org.springframework.data.gemfire.config.annotation.EnablePdx
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@CacheServerApplication(name = "BookExampleServerApplication")
+@EnableLocator
+@EnableManager(start = true)
+@EnablePdx(readSerialized = true)
+@SuppressWarnings("unused")
+public class GeodeServerApplication {
+
+  public static void main(String[] args) {
+
+    new SpringApplicationBuilder(GeodeServerApplication.class)
+      .web(WebApplicationType.NONE)
+      .build()
+      .run(args);
+  }
+}
