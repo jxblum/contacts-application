353f612929eba682d25df416717b4ca08ad96fa3
John Blum <jblum@pivotal.io>
2018-11-14 23:38:29 -0800

    Add Spring Boot configured and bootstrapped Apache Geode, CacheServer application with an embedded Locator and Manager, configured with Spring Session and Geode DataSerialization along with setting PDX read-serialized to true.

---
+++ spring-session-example/src/main/java/example/app/geode/cache/server/SessionExampleServerApplication.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package example.app.geode.cache.server;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.gemfire.config.annotation.CacheServerApplication;
+import org.springframework.data.gemfire.config.annotation.EnableLocator;
+import org.springframework.data.gemfire.config.annotation.EnableManager;
+import org.springframework.data.gemfire.config.annotation.PeerCacheConfigurer;
+import org.springframework.session.data.gemfire.config.annotation.web.http.EnableGemFireHttpSession;
+import org.springframework.session.data.gemfire.config.annotation.web.http.GemFireHttpSessionConfiguration;
+
+/**
+ * The SessionExampleServerApplication class...
+ *
+ * @author John Blum
+ * @since 1.0.0
+ */
+@SpringBootApplication
+@CacheServerApplication(name = "SessionExampleServerApplication")
+@EnableGemFireHttpSession(
+  regionName = "Sessions",
+  maxInactiveIntervalInSeconds = 180,
+  sessionSerializerBeanName = GemFireHttpSessionConfiguration.SESSION_DATA_SERIALIZER_BEAN_NAME
+)
+@EnableLocator
+@EnableManager(start = true)
+//@EnablePdx(readSerialized = true)
+@SuppressWarnings("unused")
+public class SessionExampleServerApplication {
+
+  public static void main(String[] args) {
+
+    new SpringApplicationBuilder(SessionExampleServerApplication.class)
+      .web(WebApplicationType.NONE)
+      .build()
+      .run(args);
+  }
+
+  @Bean
+  PeerCacheConfigurer pdxReadSerializedConfigurer() {
+    return (beanName, cacheFactoryBean) -> cacheFactoryBean.setPdxReadSerialized(true);
+  }
+}
