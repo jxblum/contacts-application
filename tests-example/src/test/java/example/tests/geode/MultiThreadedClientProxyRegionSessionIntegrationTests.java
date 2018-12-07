c6c9bacd10524c0582602c38b5ee9916830edf48
John Blum <jblum@pivotal.io>
2018-12-07 12:28:43 -0800

    Rename SessionClientProxyRegionIntegrationTests to MultiThreadedClientProxyRegionSessionIntegrationTests.
    Add a Session Access/Reader Thread.
    
    Rename internal test helper methods.
    
    Polish.

--- tests-example/src/test/java/example/tests/geode/MultiThreadedClientProxyRegionSessionIntegrationTests.java
+++ tests-example/src/test/java/example/tests/geode/MultiThreadedClientProxyRegionSessionIntegrationTests.java
@@ -18,23 +18,16 @@ package example.tests.geode;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.io.DataInput;
-import java.io.DataOutput;
-import java.io.IOException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Optional;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
@@ -47,26 +40,21 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.apache.geode.DataSerializable;
-import org.apache.geode.DataSerializer;
-import org.apache.geode.Delta;
-import org.apache.geode.InvalidDeltaException;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
 
 /**
- * The SessionClientProxyRegionIntegrationTests class...
+ * The MultiThreadedClientProxyRegionSessionIntegrationTests class...
  *
  * @author John Blum
  * @since 1.0.0
  */
 @SuppressWarnings("unused")
-public class SessionClientProxyRegionIntegrationTests {
+public class MultiThreadedClientProxyRegionSessionIntegrationTests {
 
-  private static final boolean ALLOW_JAVA_SERIALIZATION = false;
-  private static final boolean COPY_ON_READ = false;
+  private static final boolean COPY_ON_READ = true;
   private static final boolean SESSION_REFERENCE_CHECKING_ENABLED = false;
 
   private static final int LOCATOR_PORT = 10334;
@@ -75,7 +63,7 @@ public class SessionClientProxyRegionIntegrationTests {
 
   private static final ClientRegionShortcut CLIENT_REGION_SHORTCUT = ClientRegionShortcut.PROXY;
 
-  private static final String LOG_LEVEL = "error";
+  private static final String GEMFIRE_LOG_LEVEL = "error";
   private static final String LOCATOR_HOST = "localhost";
 
   private final AtomicInteger sessionReferenceCounter = new AtomicInteger(0);
@@ -91,9 +79,13 @@ public class SessionClientProxyRegionIntegrationTests {
 
   private Region<String, Session> sessions;
 
+  private final Set<Integer> sessionReferences = Collections.synchronizedSet(new HashSet<>(WORKLOAD_SIZE));
+
+  /*
   private final Set<Session> sessionReferences =
     Collections.synchronizedSet(new TreeSet<>((sessionOne, sessionTwo) -> sessionOne == sessionTwo ? 0
       : sessionReferenceCounter.incrementAndGet() % 2 == 0 ? -1 : 1));
+  */
 
   @Before
   public void setupGemFireClient() {
@@ -105,7 +97,7 @@ public class SessionClientProxyRegionIntegrationTests {
       .setPoolSubscriptionEnabled(true)
       .create();
 
-    // TODO - REALLY!? See GEODE-? (Bug yet to be filed).
+    // TODO - REALLY?! See GEODE-6152.
     this.clientCache.setCopyOnRead(COPY_ON_READ);
 
     this.sessions = this.clientCache.<String, Session>createClientRegionFactory(CLIENT_REGION_SHORTCUT)
@@ -123,7 +115,7 @@ public class SessionClientProxyRegionIntegrationTests {
     Properties gemfireProperties = new Properties();
 
     gemfireProperties.setProperty("name", "SessionClient");
-    gemfireProperties.setProperty("log-level", LOG_LEVEL);
+    gemfireProperties.setProperty("log-level", GEMFIRE_LOG_LEVEL);
 
     return gemfireProperties;
   }
@@ -152,6 +144,19 @@ public class SessionClientProxyRegionIntegrationTests {
       .ifPresent(ClientCache::close);
   }
 
+  private void assertUniqueSessionReference(Session session) {
+
+    if (SESSION_REFERENCE_CHECKING_ENABLED) {
+
+      int sessionIdentityHashCode = System.identityHashCode(session);
+
+      assertThat(this.sessionReferences.add(sessionIdentityHashCode))
+        .describedAs("Session reference [%d] already exist; Set size is [%d]",
+          sessionIdentityHashCode, this.sessionReferences.size())
+        .isTrue();
+    }
+  }
+
   private Session findById(String id) {
 
     return Optional.ofNullable(this.sessions.get(id))
@@ -172,7 +177,7 @@ public class SessionClientProxyRegionIntegrationTests {
     return session;
   }
 
-  private ExecutorService newSessionBatchWorkloadExecutor() {
+  private ExecutorService newSessionWorkloadExecutor() {
 
     return Executors.newFixedThreadPool(THREAD_COUNT, runnable -> {
 
@@ -186,15 +191,19 @@ public class SessionClientProxyRegionIntegrationTests {
     });
   }
 
-  private Collection<Callable<Integer>> newSessionBatchWorkload() {
+  private Collection<Callable<Integer>> newSessionWorkloadTasks() {
 
-    Collection<Callable<Integer>> sessionBatchWorkload = new ArrayList<>(WORKLOAD_SIZE);
+    Collection<Callable<Integer>> sessionWorkloadTasks = new ArrayList<>(WORKLOAD_SIZE);
 
     for (int count = 0; count < WORKLOAD_SIZE; count++) {
-      sessionBatchWorkload.add(count % 79 != 0 ? newAddSessionAttributeTask() : newRemoveSessionAttributeTask());
+      sessionWorkloadTasks.add(count % 79 != 0
+        ? newAddSessionAttributeTask()
+        : count % 237 != 0
+        ? newRemoveSessionAttributeTask()
+        : newSessionReaderTask());
     }
 
-    return sessionBatchWorkload;
+    return sessionWorkloadTasks;
   }
 
   private Callable<Integer> newAddSessionAttributeTask() {
@@ -205,14 +214,10 @@ public class SessionClientProxyRegionIntegrationTests {
 
       Session session = findById(this.sessionId.get());
 
+      assertThat(session).isNotNull();
       assertThat(session.getId()).isEqualTo(this.sessionId.get());
       assertThat(session.getLastAccessedTime()).isAfterOrEqualTo(beforeLastAccessedTime);
-
-      if (SESSION_REFERENCE_CHECKING_ENABLED) {
-        assertThat(this.sessionReferences.add(session))
-          .describedAs("Session Reference Count [%s]", this.sessionReferences.size())
-          .isTrue();
-      }
+      assertUniqueSessionReference(session);
 
       String name = UUID.randomUUID().toString();
       Object value = System.currentTimeMillis();
@@ -237,14 +242,10 @@ public class SessionClientProxyRegionIntegrationTests {
 
       Session session = findById(this.sessionId.get());
 
+      assertThat(session).isNotNull();
       assertThat(session.getId()).isEqualTo(this.sessionId.get());
       assertThat(session.getLastAccessedTime()).isAfterOrEqualTo(beforeLastAccessedTime);
-
-      if (SESSION_REFERENCE_CHECKING_ENABLED) {
-        assertThat(this.sessionReferences.add(session))
-          .describedAs("Session Reference Count [%s]", this.sessionReferences.size())
-          .isTrue();
-      }
+      assertUniqueSessionReference(session);
 
       String attributeName = null;
 
@@ -276,6 +277,25 @@ public class SessionClientProxyRegionIntegrationTests {
     };
   }
 
+  private Callable<Integer> newSessionReaderTask() {
+
+    return () -> {
+
+      Instant beforeLastAccessedTime = Instant.now();
+
+      Session session = findById(this.sessionId.get());
+
+      assertThat(session).isNotNull();
+      assertThat(session.getId()).isEqualTo(this.sessionId.get());
+      assertThat(session.getLastAccessedTime()).isAfterOrEqualTo(beforeLastAccessedTime);
+      assertUniqueSessionReference(session);
+
+      save(session.touch());
+
+      return 0;
+    };
+  }
+
   private int safeFutureGet(Future<Integer> future) {
 
     try {
@@ -286,13 +306,13 @@ public class SessionClientProxyRegionIntegrationTests {
     }
   }
 
-  private int runSessionBatchWorkloadExecutor() throws InterruptedException {
+  private int runSessionWorkload() throws InterruptedException {
 
-    ExecutorService sessionBatchWorkloadExecutor = newSessionBatchWorkloadExecutor();
+    ExecutorService sessionBatchWorkloadExecutor = newSessionWorkloadExecutor();
 
     try {
 
-      Collection<Future<Integer>> results = sessionBatchWorkloadExecutor.invokeAll(newSessionBatchWorkload());
+      Collection<Future<Integer>> results = sessionBatchWorkloadExecutor.invokeAll(newSessionWorkloadTasks());
 
       return results.stream()
         .mapToInt(this::safeFutureGet)
@@ -306,7 +326,7 @@ public class SessionClientProxyRegionIntegrationTests {
   @Test
   public void concurrentSessionAccessIsCorrect() throws InterruptedException {
 
-    int sessionAttributeCount = runSessionBatchWorkloadExecutor();
+    int sessionAttributeCount = runSessionWorkload();
 
     assertThat(sessionAttributeCount).isEqualTo(this.existingSessionAttributeNames.size());
 
@@ -315,195 +335,5 @@ public class SessionClientProxyRegionIntegrationTests {
     assertThat(session).isNotNull();
     assertThat(session.getId()).isEqualTo(this.sessionId.get());
     assertThat(session.getAttributeNames()).hasSize(sessionAttributeCount);
-  }
-
-  public static class Session implements Comparable<Session>, DataSerializable, Delta {
-
-    public static Session create() {
-      return new Session();
-    }
-
-    private transient boolean delta = false;
-
-    private Instant creationTime;
-    private Instant lastAccessedTime;
-
-    private Map<String, Object> attributes = new HashMap<>();
-    private Map<String, Object> attributeDeltas = new HashMap<>();
-
-    private String id;
-
-    public Session() {
-
-      this.id = UUID.randomUUID().toString();
-      this.creationTime = Instant.now();
-      this.lastAccessedTime = this.creationTime;
-      this.delta = true;
-    }
-
-    public synchronized String getId() {
-      return this.id;
-    }
-
-    public synchronized Object setAttribute(String name, Object value) {
-
-      return value != null
-        ? doSetAttribute(name, value)
-        : removeAttribute(name);
-    }
-
-    private Object doSetAttribute(String name, Object value) {
-
-      Object previousValue = this.attributes.put(name, value);
-
-      if (!value.equals(previousValue)) {
-        this.attributeDeltas.put(name, value);
-        this.delta = true;
-      }
-
-      return previousValue;
-    }
-
-    public synchronized Object removeAttribute(String name) {
-
-      if (this.attributes.containsKey(name)) {
-        this.attributeDeltas.put(name, null);
-        this.delta = true;
-      }
-
-      return this.attributes.remove(name);
-    }
-
-    public synchronized Object getAttribute(String name) {
-      return this.attributes.get(name);
-    }
-
-    public synchronized Set<String> getAttributeNames() {
-      return Collections.unmodifiableSet(new HashSet<>(this.attributes.keySet()));
-    }
-
-    public synchronized Instant getCreationTime() {
-      return this.creationTime;
-    }
-
-    public synchronized Instant getLastAccessedTime() {
-      return this.lastAccessedTime;
-    }
-
-    public synchronized Session commit() {
-
-      this.delta = false;
-      this.attributeDeltas.clear();
-
-      return this;
-    }
-
-    public synchronized Session touch() {
-
-      Instant newLastAccessedTime = Instant.now();
-
-      this.delta |= !newLastAccessedTime.equals(this.lastAccessedTime);
-      this.lastAccessedTime = newLastAccessedTime;
-
-      return this;
-    }
-
-    @Override
-    public synchronized void toData(DataOutput out) throws IOException {
-
-      out.writeUTF(this.getId());
-      out.writeLong(this.getCreationTime().toEpochMilli());
-      out.writeLong(this.getLastAccessedTime().toEpochMilli());
-      out.writeInt(this.attributes.size());
-
-      for (Entry<String, Object> entry : this.attributes.entrySet()) {
-        out.writeUTF(entry.getKey());
-        DataSerializer.writeObject(entry.getValue(), out, ALLOW_JAVA_SERIALIZATION);
-      }
-    }
-
-    @Override
-    public void fromData(DataInput in) throws IOException, ClassNotFoundException {
-
-      this.id = in.readUTF();
-      this.creationTime = Instant.ofEpochMilli(in.readLong());
-      this.lastAccessedTime = Instant.ofEpochMilli(in.readLong());
-
-      for (int count = in.readInt(); count > 0; count--) {
-        setAttribute(in.readUTF(), DataSerializer.readObject(in));
-      }
-    }
-
-    @Override
-    public synchronized void toDelta(DataOutput out) throws IOException {
-
-      out.writeLong(this.getLastAccessedTime().toEpochMilli());
-      out.writeInt(this.attributeDeltas.size());
-
-      for (Entry<String, Object> entry : this.attributeDeltas.entrySet()) {
-        out.writeUTF(entry.getKey());
-        DataSerializer.writeObject(entry.getValue(), out, ALLOW_JAVA_SERIALIZATION);
-      }
-    }
-
-    @Override
-    public void fromDelta(DataInput in) throws IOException, InvalidDeltaException {
-
-      String key = null;
-
-      try {
-
-        this.lastAccessedTime = Instant.ofEpochMilli(in.readLong());
-
-        for (int count = in.readInt(); count > 0; count--) {
-          key = in.readUTF();
-          setAttribute(key, DataSerializer.readObject(in));
-        }
-      }
-      catch (ClassNotFoundException cause) {
-        throw new IOException(String.format("Failed to resolve type in delta for attribute [%s]", key), cause);
-      }
-    }
-
-    @Override
-    public synchronized boolean hasDelta() {
-      return this.delta || !this.attributeDeltas.isEmpty();
-    }
-
-    @Override
-    public int compareTo(Session session) {
-      return this.getId().compareTo(session.getId());
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-
-      if (this == obj) {
-        return true;
-      }
-
-      if (!(obj instanceof Session)) {
-        return false;
-      }
-
-      Session that = (Session) obj;
-
-      return this.getId().equals(that.getId());
-    }
-
-    @Override
-    public int hashCode() {
-
-      int hashValue = 17;
-
-      hashValue = 37 * hashValue + getId().hashCode();
-
-      return hashValue;
-    }
-
-    @Override
-    public String toString() {
-      return getId();
-    }
   }
 }
